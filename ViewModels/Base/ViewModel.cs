fad13444397a6c100ab21517f6ca8e7a73987714
Nicolas <nikolai_vaneev@mail.ru>
2021-11-24 09:07:01 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MotoShop.Services
+{
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        //~ViewModel()
+        //{
+        //    Dispose(false);
+        //}
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            // Осбобождаем ресурсы.
+        }
+    }
+}
---
+++ ViewModels/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using MotoShop.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace MotoShop.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
